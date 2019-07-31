cbcaaa6f096c8b627646ca9ca8fb13aab9c922ce
inkyu94 <sexyik@naver.com>
2019-07-31 17:00:42 +0900

    hi

--- src/main/java/kr/or/ddit/servlet/Test2.java
+++ src/main/java/kr/or/ddit/servlet/Test2.java
@@ -15,6 +15,7 @@ public class Test2 extends HttpServlet {
 	Date date = new Date();
 	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
+	// dkanrjsk
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html; charset=utf-8");
